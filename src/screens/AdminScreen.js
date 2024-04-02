a1a39a57c7f105f9762430f22b6e78bdd80eac42
Srishti168 <srishti1385.be21@chitkara.edu.in>
2024-04-02 23:47:50 +0530

    Update AdminScreen.js

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -1,48 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { Tabs } from "antd";
-import AdminBookingScreen from "./AdminBookingScreen";
-import AdminRoomScreen from "./AdminRoomScreen";
-import AdminUserScreen from "./AdminUserScreen";
-import AdminAddRoomScreen from "./AdminAddRoomScreen";
+import axios from "axios";
+import { Table, Tag } from "antd";
+import Loader from "../components/Loader";
+import Error from "../components/Error";
 
-const { TabPane } = Tabs;
+function AdminUserScreen() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-function callback(key) {
-  console.log(key);
-}
+  const columns = [
+    { title: "userid", dataIndex: "_id", key: "_id", width: 150 },
+    { title: "Name", dataIndex: "name", key: "name", width: 200 },
+    { title: "Email", dataIndex: "email", key: "email", width: 250 },
+    {
+      title: "isAdmin",
+      dataIndex: "isAdmin",
+      key: "isAdmin",
+      render: (isAdmin) => (
+        <>
+          {isAdmin === true ? (
+            <Tag color="green">YES</Tag>
+          ) : (
+            <Tag color="red">NO</Tag>
+          )}
+        </>
+      ),
+      width: 100,
+    },
+  ];
 
-function AdminScreen() {
-  const user = JSON.parse(localStorage.getItem("currentUser"));
+  async function fetchUsers() {
+    setError("");
+    setLoading(true);
+    try {
+      const data = (await axios.post("https://mern-project-6.onrender.com/api/users/getallusers")).data;
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+    setLoading(false);
+  }
 
   useEffect(() => {
-    if (!user || user.isAdmin === false) {
-      window.location.href = "/home";
-    }
+    fetchUsers();
   }, []);
 
   return (
-    <div className="container mt-3">
-      <h1 className="text-center mb-4">Admin Panel</h1>
-      <div className="row justify-content-center">
-        <div className="col-lg-10">
-          <Tabs defaultActiveKey="1" onChange={callback}>
-            <TabPane tab="Bookings" key="1">
-              <AdminBookingScreen />
-            </TabPane>
-            <TabPane tab="Rooms" key="2">
-              <AdminRoomScreen />
-            </TabPane>
-            <TabPane tab="Add Room" key="3">
-              <AdminAddRoomScreen />
-            </TabPane>
-            <TabPane tab="Users" key="4">
-              <AdminUserScreen />
-            </TabPane>
-          </Tabs>
+    <div className="row">
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Error msg={error} />
+      ) : (
+        <div className="col-md-12">
+          <div className="table-responsive">
+            <Table columns={columns} dataSource={users} scroll={{ x: true }} />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
 
-export default AdminScreen;
+export default AdminUserScreen;
