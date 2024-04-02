402067f0708cf5381efbcd6d88c9aa2a207f6c92
Srishti168 <srishti1385.be21@chitkara.edu.in>
2024-04-02 23:37:36 +0530

    Update AdminScreen.js

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -7,10 +7,6 @@ import AdminAddRoomScreen from "./AdminAddRoomScreen";
 
 const { TabPane } = Tabs;
 
-function callback(key) {
-  console.log(key);
-}
-
 function AdminScreen() {
   const user = JSON.parse(localStorage.getItem("currentUser"));
 
@@ -20,27 +16,40 @@ function AdminScreen() {
     }
   }, []);
 
+  function callback(key) {
+    console.log(key);
+  }
+
   return (
     <div className="container mt-3">
       <h1 className="text-center mb-4">Admin Panel</h1>
       <div className="row justify-content-center">
         <div className="col-lg-8 col-md-10">
-          <Tabs defaultActiveKey="1" onChange={callback}>
-            <TabPane tab="Bookings" key="1">
-              <AdminBookingScreen />
-            </TabPane>
-            <TabPane tab="Rooms" key="2">
-              <AdminRoomScreen />
-            </TabPane>
-            <TabPane tab="Add Room" key="3">
-              <AdminAddRoomScreen />
-            </TabPane>
-            <TabPane tab="Users" key="4">
-              <AdminUserScreen />
-            </TabPane>
-          </Tabs>
+          <div className="admin-tabs-container">
+            <Tabs defaultActiveKey="1" onChange={callback}>
+              <TabPane tab="Bookings" key="1">
+                <AdminBookingScreen />
+              </TabPane>
+              <TabPane tab="Rooms" key="2">
+                <AdminRoomScreen />
+              </TabPane>
+              <TabPane tab="Add Room" key="3">
+                <AdminAddRoomScreen />
+              </TabPane>
+              <TabPane tab="Users" key="4">
+                <AdminUserScreen />
+              </TabPane>
+            </Tabs>
+          </div>
         </div>
       </div>
+      <style>
+        {`
+        .admin-tabs-container .ant-tabs-content {
+          overflow-x: auto;
+        }
+        `}
+      </style>
     </div>
   );
 }
