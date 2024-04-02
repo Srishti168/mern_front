6097c0783a7fd05b1b4aeadbf61cdb187b585d88
Srishti168 <srishti1385.be21@chitkara.edu.in>
2024-04-02 23:23:19 +0530

    update

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-
 import { Tabs } from "antd";
 import AdminBookingScreen from "./AdminBookingScreen";
 import AdminRoomScreen from "./AdminRoomScreen";
 import AdminUserScreen from "./AdminUserScreen";
 import AdminAddRoomScreen from "./AdminAddRoomScreen";
+
 const { TabPane } = Tabs;
+
 function callback(key) {
   console.log(key);
 }
+
 function AdminScreen() {
   const user = JSON.parse(localStorage.getItem("currentUser"));
 
@@ -19,20 +21,20 @@ function AdminScreen() {
   }, []);
 
   return (
-    <div className="ml-3 mt-3 mr-3 bs">
-      <h1 className="text-center">Admin Panel</h1>
+    <div className="container mt-3">
+      <h1 className="text-center mb-4">Admin Panel</h1>
       <Tabs defaultActiveKey="1" onChange={callback}>
         <TabPane tab="Bookings" key="1">
-          <AdminBookingScreen></AdminBookingScreen>
+          <AdminBookingScreen />
         </TabPane>
         <TabPane tab="Rooms" key="2">
-          <AdminRoomScreen></AdminRoomScreen>
+          <AdminRoomScreen />
         </TabPane>
         <TabPane tab="Add Room" key="3">
-          <AdminAddRoomScreen></AdminAddRoomScreen>
+          <AdminAddRoomScreen />
         </TabPane>
         <TabPane tab="Users" key="4">
-          <AdminUserScreen></AdminUserScreen>
+          <AdminUserScreen />
         </TabPane>
       </Tabs>
     </div>
